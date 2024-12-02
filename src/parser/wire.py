9d2d9720fdd1730b45aea6bc8589bf28b71529b8
Chengjia Liu <143008726+chengjialiu@users.noreply.github.com>
2024-12-02 14:05:57 -0600

    Logic change for net extraction

--- src/parser/wire.py
+++ src/parser/wire.py
@@ -1,7 +1,7 @@
 import csv
 
-input_path = "/home/grads/c/chengjialiu/rl_proj/n300.nets"
-output_path = "/home/grads/c/chengjialiu/rl_proj/n300_nets.csv"
+input_path = "/home/grads/c/chengjialiu/rl_proj/n50.nets"
+output_path = "/home/grads/c/chengjialiu/rl_proj/n50_nets.csv"
 
 # Read data
 rows = []
@@ -12,26 +12,21 @@ with open(input_path, "r") as file:
     for line in file:
         line = line.strip()
         if line.startswith("NetDegree"):
-            # if met new NetDegree, save the current row and start a new row
-            if current_row:
-                rows.append(current_row)
-            current_row = [f"net{net_count}"]
+            # # if met new NetDegree, save the current row and start a new row
+            # if current_row:
+            #     rows.append(current_row)
+            current_row = f"net{net_count}"
             net_count += 1
-        # if the line is a block name
         elif line.startswith("sb") or line.startswith("p"):  
             blk_name = line.split()[0].strip()
-            current_row.append(blk_name)
-    
-    # add the last row
-    if current_row:
-        rows.append(current_row)
+            rows.append([current_row, blk_name])
+
 
 # Save to CSV
 with open(output_path, "w", newline="") as csvfile:
     writer = csv.writer(csvfile)
     # Create header
-    max_columns = max(len(row) for row in rows)
-    header = ["name"] + [f"blk{i}" for i in range(1, max_columns)]
+    header = ["net", "blk"]
     writer.writerow(header)
     # Write data
     writer.writerows(rows)
